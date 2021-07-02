3036a58d85002c623bb01704d8b35154f8af9f2e
Curtis H <sitrucrettah1@gmail.com>
2021-07-01 18:30:10 -0700

    Add class person and instantiate

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Let's create a class of Person
+
+function Person(name){
+    this.name = name;
+    this.greeting = function(){
+        console.log(`Hi! I'm ${this.name}.`);
+    };
+};
+
+var person1 = new Person("Curtis");
+var person2 = new Person("Savannah");
+console.log(person1.name);
+person1.greeting();
+
