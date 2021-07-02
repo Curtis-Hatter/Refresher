c2d64b283e013fa82f3a4a34013089a5552123bc
Curtis H <sitrucrettah1@gmail.com>
2021-07-01 18:38:25 -0700

    Add complex constructor

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 // Let's create a class of Person
 
-function Person(name){
-    this.name = name;
-    this.greeting = function(){
-        console.log(`Hi! I'm ${this.name}.`);
-    };
-};
+// function Person(name){
+//     this.name = name;
+//     this.greeting = function(){
+//         console.log(`Hi! I'm ${this.name}.`);
+//     };
+// };
 
-var person1 = new Person("Curtis");
-var person2 = new Person("Savannah");
+// Create a more difficult constructor
+function Person(first, last, age, gender, interests){
+    this.name={
+        first: first,
+        last : last
+    };
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
+    this.bio = function(){
+        console.log(`Hello, my name is ${this.name.first} ${this.name.last}, I'm ${this.age} years old and I like ${this.interests}!`);
+    };
+    this.greeting = function(){
+        console.log(`Hello, my name is ${this.name.first} ${this.name.last}.`);
+    };
+}
+
+
+var person1 = new Person("Curtis", "Hatter", 27, 'M', "Coding");
+// var person2 = new Person("Savannah");
 console.log(person1.name);
+person1.bio();
 person1.greeting();
 
